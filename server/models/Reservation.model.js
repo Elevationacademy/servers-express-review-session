5957c7e32fa5ba244e2dde3dabdda1a4dbad63d2
danbrud <dannybrudner@users.noreply.github.com>
2020-12-02 12:19:25 +0200

    full code

--- server/models/Reservation.model.js
+++ server/models/Reservation.model.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const reservationSchema = new Schema({
-
+  name: String,
+  numPeople: Number,
+  date: Date,
+  isComplete: Boolean
 })
 
 const Reservation = mongoose.model('Reservation', reservationSchema)
--- server/routes/api.js
+++ server/routes/api.js
@@ -6,4 +6,46 @@ router.get('/sanity', (req, res) => {
   res.send('All good!')
 })
 
+// Create a new reservation
+router.post('/reservations', (req, res) => {
+  const reservation = new Reservation({ ...req.body, isComplete: false })
+
+  reservation
+    .save()
+    .then(reservation => {
+      res.send(reservation)
+    })
+    .catch(err => {
+      res.send(err)
+    })
+})
+
+// Get all reservations
+router.get('/reservations', async (req, res) => {
+  try {
+    const reservations = await Reservation.find({})
+    res.send(reservations)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
+// Delete a reservation
+router.delete('/reservations/:resId', async (req, res) => {
+  const { resId } = req.params
+
+  const reservation = await Reservation.findByIdAndDelete(resId)
+  res.send(reservation)
+})
+
+// Update a reservation
+router.put('/reservations/:resId', async (req, res) => {
+  const { resId } = req.params
+  const { property, value } = req.body
+
+  const reservation = await Reservation
+    .findByIdAndUpdate(resId, { [property]: value }, { new: true })
+  res.send(reservation)
+})
+
 module.exports = router
